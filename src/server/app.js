223806bab57df090382ea7cb2a16dce0b3af3076
Daniel Klein <danhirschklein@gmail.com>
2016-03-28 18:35:09 -0600

    Set up books routes and views

--- src/server/app.js
+++ src/server/app.js
@@ -10,6 +10,7 @@ var swig = require('swig');
 
 // *** routes *** //
 var routes = require('./routes/index.js');
+var books = require('./routes/books.js');
 
 
 // *** express instance *** //
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, '../client')));
 
 // *** main routes *** //
 app.use('/', routes);
+app.use('/books', books);
 
 
 // catch 404 and forward to error handler
---
+++ src/server/routes/books.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+var knex = require('knex');
+//var queries = require("../../../queries");
+
+//Render Book list
+router.get('/', function(req, res, next){
+
+  var promises = [];
+
+  promises.push(queries.getAllBooks());
+
+  promises.push(queries.getAllAuthors());
+
+  return Promise.all(promises)
+
+  .then( function (result) {
+    res.render('books', { title: 'Galvanize Reads | Books',
+                          books: result[0],
+                          authors: result[1].rows }
+    );
+  })
+
+  .catch( function (error) { console.log(error); return error; });
+
+});
+// //Show form to create new book
+// router.get('/new', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// //Show form to edit single book
+// router.get('/edit/:id', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// //Show Single Book
+// router.get('/:id', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// //Post form to create new book
+// router.post('/new')
+
+// //Delete single book
+// router.post('/delete/:id')
+
+// //Edit single book
+// router.post('/edit/:id')
+module.exports = router;
